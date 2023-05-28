e1f65688edcc8ab33e8c6633b23e47872c7db456
TRAN <minhtuan46@hotmail.fr>
2023-05-28 19:27:30 +0200

    petit bug enfin trouvé

--- projects/playground/src/app/hightlight.directive.ts
+++ projects/playground/src/app/hightlight.directive.ts
@@ -1,26 +1,30 @@
-import { Directive, HostBinding, HostListener, Input } from "@angular/core";
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
-selector:'p[highlight]',
+  selector: 'p[highlight]',
 })
 
 export class HighlightDirective {
-    @HostBinding('style.backgroundColor')
-    color= 'transparent';
+  @HostBinding('style.backgroundColor')
+  color = 'transparent';
 
-    @Input('background-color')
-    backgroundColor = 'yellow';
+  ngOnInit() {
+    this.color = this.baseColor;
+  }
 
-    @Input('base-color')
-    baseColor = 'transparent';
+  @Input('background-color')
+  backgroundColor = 'yellow';
 
-    @HostListener('mouseenter')
-    onMouseEnter() {
-        this.color = this.backgroundColor;
-    }
+  @Input('base-color')
+  baseColor = 'transparent';
 
-    @HostListener('mouseout')
-    onMouseOut() {
-        this.color = this.baseColor;
-    }
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.color = this.backgroundColor;
+  }
+
+  @HostListener('mouseout')
+  onMouseOut() {
+    this.color = this.baseColor;
+  }
 }
